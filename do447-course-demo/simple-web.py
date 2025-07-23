68f89dec6cb8aa5889157b80bc3772f1c62c8c6d
Alberthua-Perl <hualongfeiyyy@163.com>
2025-07-23 23:12:52 +0800

    Update do374 associated playbooks completely

--- do447-course-demo/simple-web.py
+++ do447-course-demo/simple-web.py
@@ -9,6 +9,7 @@ listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
 print('Serving HTTP on port %s ...' % PORT)
+
 while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(1024)
